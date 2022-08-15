c2437763698446b74bf3b80ce820ef63eb7ab0fa
NewajShovo <leoshovo@gmail.com>
2022-08-15 10:23:05 +0600

    Linking webpage inside a page done

--- First_Project/First_Project/urls.py
+++ First_Project/First_Project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from first_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/',views.index, name='index'),
-    path('',views.index, name='index'),
+    path('',views.home, name='home'),
+    path('contact/',views.contact, name='contact'),
+    path('about/', views.about, name='about'),
 ]
--- First_Project/first_app/views.py
+++ First_Project/first_app/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("<h1>Hello world!</h1>")
+def home(request):
+    return HttpResponse("<h1>This is home page</h1> <a href='contact/'>Contact</a> <a href='about/'>About</a>")
+
+def contact(request):
+    return HttpResponse("<h1>This is contact Page</h1> <a href='/'>HomePage</a> <a href='/about/'>About</a>")
+
+def about(request):
+    return HttpResponse("<h1>About Us</h1> <a href='/'>HomePage</a> <a href='/contact/'>Contact</a>")
