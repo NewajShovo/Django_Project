82b544646fbad7147602c1f5b08d119aa2ba560b
NewajShovo <leoshovo@gmail.com>
2022-08-28 12:40:21 +0600

    1. Creating Models 2.Command related to migrate 3.Inserting value in it

--- First_Project/first_app/models.py
+++ First_Project/first_app/models.py
@@ -1,3 +1,20 @@
+from operator import truediv
+from unicodedata import name
 from django.db import models
 
-# Create your models here.
+class Musician(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    instrument = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.first_name+" "+self.last_name
+
+class Album (models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+
